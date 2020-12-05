91807bf4960bb92c4a85aba4c9ba6d1578cf4683
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-12-05 14:15:25 -0800

    feat(migrations): create migrations
    added user and product migrations

---
+++ server/data/migrations/20201205135553_products.ts
@@ -0,0 +1,56 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+   return knex.schema.createTable("product", table => {
+      table.increments();
+      table.string("name",255).notNullable();
+      table.integer("price").notNullable();
+   })
+   .createTable("sizes", table => {
+      table.increments();
+      table.enu('size', ["xxl", "xl", "large", "medium", "small"])
+         .notNullable();
+   })
+   .createTable("images", table => {
+      table.increments();
+      table.string("img_url");
+   })
+   .createTable("product_size", table => {
+      table.increments();
+      table.integer("size_id")
+         .unsigned()
+         .notNullable()
+         .references("sizes.id")
+         .onUpdate("CASCADE")
+         .onDelete("CASCADE");
+      table.integer("product_id")
+         .unsigned()
+         .notNullable()
+         .references("product.id")
+         .onUpdate("CASCADE")
+         .onDelete("CASCADE");
+   })
+   .createTable("product_img", table => {
+      table.increments();
+      table.integer("product_id")
+         .unsigned()
+         .notNullable()
+         .references("product.id")
+         .onUpdate("CASCADE")
+         .onDelete("CASCADE");
+      table.integer("img_id")
+         .unsigned()
+         .notNullable()
+         .references("images.id")
+         .onUpdate("CASCADE")
+         .onDelete("CASCADE");
+   })
+};
+
+export async function down(knex: Knex): Promise<any> {
+   return knex.schema
+      .dropTableIfExists("product_img")
+      .dropTableIfExists("product_size")
+      .dropTableIfExists("images")
+      .dropTableIfExists("product");
+};
