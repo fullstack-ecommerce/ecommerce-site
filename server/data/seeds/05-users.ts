34e0c26cdfb22dc5bc6d53f613956e2b3cc7b0ef
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-12-08 19:25:10 -0800

    refactor(seeds): hash password for testing

--- server/data/seeds/05-users.ts
+++ server/data/seeds/05-users.ts
@@ -1,4 +1,5 @@
 import * as Knex from 'knex';
+import bcrypt from 'bcryptjs';
 
 export async function seed(knex: Knex) {
   // Deletes ALL existing entries
@@ -7,9 +8,24 @@ export async function seed(knex: Knex) {
     .then(async function () {
       // Inserts seed entries
       return knex('users').insert([
-        {username: "user One", email: "userone@gmail.com",password: "useronepass", is_admin: false},
-        {username: "user Two", email: "usertwo@gmail.com",password: "usertwopass", is_admin: false},
-        {username: "user Three", email: "userthree@gmail.com",password: "userthreepass", is_admin: false},
+        {
+            username: "user One", 
+            email: "userone@gmail.com",
+            password: await bcrypt.hash("password", 8), 
+            is_admin: false
+         },
+        {
+            username: "user Two", 
+            email: "usertwo@gmail.com",
+            password: "usertwopass", 
+            is_admin: false
+         },
+        {
+            username: "user Three", 
+            email: "userthree@gmail.com",
+            password: "userthreepass", 
+            is_admin: false
+        },
       ]);
     });
 };
