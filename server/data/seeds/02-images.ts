5ae0d4a6e9068d4ea5eed936c09f966473bed90f
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-12-17 12:26:39 -0800

    feat(seeds): add images for products in the seeds files
    added seed images for testing

--- server/data/seeds/02-images.ts
+++ server/data/seeds/02-images.ts
@@ -7,11 +7,11 @@ export async function seed(knex: Knex) {
     .then(async function () {
       // Inserts seed entries
       return knex('images').insert([
-        {img_url: "exampleone"},
-        {img_url: "exampletwo"},
-        {img_url: "examplethree"},
-        {img_url: "examplefour"},
-        {img_url: "examplefive"},
+        {img_url: "https://cdni.llbean.net/is/image/wim/220626_683_41?hei=764&wid=665&resMode=sharp2&defaultImage=llbstage/A0211793_2"},
+        {img_url: "https://cdni.llbean.net/is/image/wim/220626_683_41?hei=764&wid=665&resMode=sharp2&defaultImage=llbstage/A0211793_2"},
+        {img_url: "https://cdni.llbean.net/is/image/wim/220626_683_41?hei=764&wid=665&resMode=sharp2&defaultImage=llbstage/A0211793_2"},
+        {img_url: "https://cdni.llbean.net/is/image/wim/220626_683_41?hei=764&wid=665&resMode=sharp2&defaultImage=llbstage/A0211793_2"},
+        {img_url: "https://cdni.llbean.net/is/image/wim/220626_683_41?hei=764&wid=665&resMode=sharp2&defaultImage=llbstage/A0211793_2"},
       ]);
     });
 };
