47d43a3a683d94bb1ca8142178557e9cfb609a31
Sone <sthaya@gmail.com>
2020-12-17 17:35:31 -0800

    adding to cart now post to backend

--- client/src/screens/ProductScreen/RenderProductScreen.js
+++ client/src/screens/ProductScreen/RenderProductScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { addToCart } from "../../state/actions/cartActions";
+import { listProductDetails } from "../../state/actions/productActions";
 import gallery1 from "../../assets/images/gallery1.jpg";
 import gallery2 from "../../assets/images/gallery2.jpg";
 import gallery3 from "../../assets/images/gallery3.jpg";
@@ -14,16 +15,21 @@ import "./productScreen.css";
 
 const initialState = {
   product_name: "",
-  price: "",
-  //size: "",
+  product_price: "",
+  product_size: "",
 };
 
-const RenderProductScreen = () => {
+const RenderProductScreen = ({ match }) => {
   const [quantity, setQuantity] = useState(0);
-  const [size, setSize] = useState("");
-
+  const [sizes, setSizes] = useState([]);
   const [productToAdd, setProductToAdd] = useState(initialState);
-  const [data, setData] = useState({});
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const {
+    loading: loadingDetails,
+    error: errorDetails,
+    product,
+  } = productDetails;
 
   const dispatch = useDispatch();
 
@@ -33,155 +39,150 @@ const RenderProductScreen = () => {
   const { userInfo } = userLogin;
 
   console.log(userInfo);
-  console.log("id", id);
+  console.log("product", product);
 
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
 
   useEffect(() => {
-    const productData = products.find((product) => product.id === Number(id));
-    setData(productData);
-    console.log("object");
-  }, [data]);
-
-  console.log(data);
+    dispatch(listProductDetails(id));
+  }, [dispatch]);
 
   const addToCartHandler = (e) => {
     e.preventDefault();
-    // need to work on this
     const newObj = {
       ...initialState,
-      user_id: 1,
-      product_price: data.price,
-      product_name: data.name,
-      product_img: data.images[0].img_url,
+      user_id: userInfo.user_id,
+      product_price: product.price,
+      product_name: product.name,
+      product_img: product.images[0].img_url,
+      product_size: product.sizes[0].size,
       quantity,
     };
-    console.log("new object", newObj);
-    console.log("from addToCart function");
 
     dispatch(addToCart(newObj));
   };
 
+  console.log(sizes);
+  console.log(quantity);
+
   return (
     <>
-      <>
-        <div className="small__container single__product">
-          <Link to="/products">
-            <button>Go Back</button>
-          </Link>
-          <div className="row__container">
-            <div className="column__one">
-              <img src={gallery1} alt="one" width="100%" />
+      <div className="small__container single__product">
+        <Link to="/products">
+          <button>Go Back</button>
+        </Link>
+        <div className="row__container">
+          <div className="column__one">
+            <img src={gallery1} alt="one" width="100%" />
 
-              {/* ?model popup to enlarge the smaller images? */}
-              <div className="small__img__row">
-                <div className="small__img__col">
-                  <img src={gallery1} alt="one" width="100%" />
-                </div>
-                <div className="small__img__col">
-                  <img src={gallery2} alt="one" width="100%" />
-                </div>
-                <div className="small__img__col">
-                  <img src={gallery3} alt="one" width="100%" />
-                </div>
-                <div className="small__img__col">
-                  <img src={gallery4} alt="one" width="100%" />
-                </div>
+            {/* ?model popup to enlarge the smaller images? */}
+            <div className="small__img__row">
+              <div className="small__img__col">
+                <img src={gallery1} alt="one" width="100%" />
+              </div>
+              <div className="small__img__col">
+                <img src={gallery2} alt="one" width="100%" />
+              </div>
+              <div className="small__img__col">
+                <img src={gallery3} alt="one" width="100%" />
+              </div>
+              <div className="small__img__col">
+                <img src={gallery4} alt="one" width="100%" />
               </div>
             </div>
-            <div className="column__one">
-              <form onSubmit={addToCartHandler}>
-                <h1>{data.name}</h1>
-                <h4>${data.price}</h4>
-                <select onChange={(e) => setSize(e.target.value)}>
-                  <option>Select Size</option>
-                  <option value="xxl">XXL</option>
-                  <option value="xl">XL</option>
-                  <option value="large">Large</option>
-                  <option value="medium">Medium</option>
-                  <option value="small">Small</option>
-                </select>
-                <input
-                  type="number"
-                  value={quantity}
-                  name="quantity"
-                  onChange={(e) => setQuantity(e.target.value)}
-                />
-                <button className="add__button" type="submit">
-                  Add To Cart
-                </button>
-                <h3>
-                  Product Details <i className="fas fa-indent"></i>
-                </h3>
-                <br />
-                <p>{data.description}</p>
-              </form>
-            </div>
+          </div>
+          <div className="column__one">
+            <form onSubmit={addToCartHandler}>
+              <h1>{product.name}</h1>
+              <h4>${product.price}</h4>
+              <select onChange={(e) => setSizes(e.target.value)}>
+                <option>Select Size</option>
+                <option value="xxl">XXL</option>
+                <option value="xl">XL</option>
+                <option value="large">Large</option>
+                <option value="medium">Medium</option>
+                <option value="small">Small</option>
+              </select>
+              <input
+                type="number"
+                value={quantity}
+                name="quantity"
+                onChange={(e) => setQuantity(e.target.value)}
+              />
+              <button className="add__button" type="submit">
+                Add To Cart
+              </button>
+              <h3>
+                Product Details <i className="fas fa-indent"></i>
+              </h3>
+              <br />
+              <p>{product.description}</p>
+            </form>
           </div>
         </div>
+      </div>
 
-        <div className="small__container">
-          <div className="row__container row__two">
-            <h2>Related Products</h2>
-            <p>View More</p>
+      <div className="small__container">
+        <div className="row__container row__two">
+          <h2>Related Products</h2>
+          <p>View More</p>
+        </div>
+      </div>
+
+      <div className="row__three__container">
+        <div className="column__three">
+          <img src={product1} alt="product 1" />
+          <h4>Red Printed T-shirt</h4>
+          <div className="rating__stars">
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="far fa-star"></i>
           </div>
+          <p>$50.00</p>
         </div>
 
-        <div className="row__three__container">
-          <div className="column__three">
-            <img src={product1} alt="product 1" />
-            <h4>Red Printed T-shirt</h4>
-            <div className="rating__stars">
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="far fa-star"></i>
-            </div>
-            <p>$50.00</p>
+        <div className="column__three">
+          <img src={product2} alt="product 2" />
+          <h4>Black Shoes</h4>
+          <div className="rating__stars">
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star-half-alt"></i>
           </div>
-
-          <div className="column__three">
-            <img src={product2} alt="product 2" />
-            <h4>Black Shoes</h4>
-            <div className="rating__stars">
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star-half-alt"></i>
-            </div>
-            <p>$70.00</p>
-          </div>
-
-          <div className="column__three">
-            <img src={product3} alt="product 3" />
-            <h4>Grey Pants</h4>
-            <div className="rating__stars">
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star-half-alt"></i>
-              <i className="far fa-star"></i>
-            </div>
-            <p>$60.00</p>
-          </div>
-
-          <div className="column__three">
-            <img src={product4} alt="product 4" />
-            <h4>Blue Printed T-shirt</h4>
-            <div className="rating__stars">
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="far fa-star"></i>
-            </div>
-            <p>$50.00</p>
-          </div>
+          <p>$70.00</p>
         </div>
-      </>
+
+        <div className="column__three">
+          <img src={product3} alt="product 3" />
+          <h4>Grey Pants</h4>
+          <div className="rating__stars">
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star-half-alt"></i>
+            <i className="far fa-star"></i>
+          </div>
+          <p>$60.00</p>
+        </div>
+
+        <div className="column__three">
+          <img src={product4} alt="product 4" />
+          <h4>Blue Printed T-shirt</h4>
+          <div className="rating__stars">
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="far fa-star"></i>
+          </div>
+          <p>$50.00</p>
+        </div>
+      </div>
     </>
   );
 };
--- client/src/state/reducers/cartReducer.js
+++ client/src/state/reducers/cartReducer.js
@@ -1,31 +1,28 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+  CART_ADD_ITEM_REQUEST,
+  CART_ADD_ITEM_SUCCESS,
+  CART_ADD_ITEM_FAIL,
+  CART_REMOVE_ITEM,
+} from "../constants/cartConstants";
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
+export const cartReducer = (state = {}, action) => {
   switch (action.type) {
-    case CART_ADD_ITEM:
-      const item = action.payload;
-
-      const existItem = state.cartItems.find((x) => x.product === item.product);
-
-      if (existItem) {
-        return {
-          ...state,
-          cartItems: state.cartItems.map((x) =>
-            x.product === existItem.product ? item : x
-          ),
-        };
-      } else {
-        return {
-          ...state,
-          cartItems: [...state.cartItems, item],
-        };
-      }
-    case CART_REMOVE_ITEM:
+    case CART_ADD_ITEM_REQUEST:
       return {
-        ...state,
-        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+        loading: true,
+      };
+    case CART_ADD_ITEM_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        cart: action.payload,
       };
 
+    case CART_ADD_ITEM_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
