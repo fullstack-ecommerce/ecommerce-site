73ff7049d83531a41e7d5b89d951f7d23d830cb6
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-12-21 19:10:40 -0800

    test

---
+++ env.example.sh
@@ -0,0 +1,6 @@
+export ROUNDS = # number of rounds to hash password
+export SECRET_JWT = # secret to hash password
+export GMAIL_USER = # email user for email services
+export GMAIL_PASS = # email password
+export RESET_PASS = # secret to reset password
+export LIENT_URL= # client URL
