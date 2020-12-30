1c1be182dde9fe77773b10bfa98d173433483d17
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-12-30 15:18:54 -0800

    refactor(send-emails): change nodemailer to sendgrid
    changed code to use SendGrid instead of Nodemailer, there were some issues with nodemailer and also it does not work on production

--- server/emailFiles/sendEmails.ts
+++ server/emailFiles/sendEmails.ts
@@ -1,41 +1,54 @@
-import nodemailer from 'nodemailer';
-import { gmailUser, gmailPassword } from '../envVariables';
+import { sendGrdiKey, sender } from '../envVariables';
+import sgMail from '@sendgrid/mail';
 
-async function resetPasswordEmail(email: string, token: string) {
-   const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-         user: gmailUser,
-         pass: gmailPassword
-      },
-   });
 
-   const info = await transporter.sendMail({
-      from: "App name",
+function resetPasswordEmail(email: string, token: string) {
+   if(!sendGrdiKey ) return null;
+   if(!sender) return null;
+
+   sgMail.setApiKey(sendGrdiKey);
+   const msg = {
       to: email,
-      subject: "Reset Password",
-      html: `<a href=${token}>click the link ${token}</a>`
-   });
-   console.log("Message sent: %s", info.messageId);
+      from: sender, 
+      subject: "Reset your Password",
+      text: 'important',
+      html: `<a href=${token}>click the link ${token}</a>`,
+   }
+   sgMail
+      .send(msg)
+      .then(() => {
+         console.log('Email sent')
+      })
+      .catch((error: any) => {
+         console.error(error)
+      })
 }
 
-async function updatedPasswordEmail(email: string) {
-   const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-         user: gmailUser,
-         pass: gmailPassword
-      },
-   });
+function updatedPasswordEmail(email: string) {
+   if(!sendGrdiKey ) return null;
 
-   const info = await transporter.sendMail({
-      from: "App name",
+   if(!sender) return null;
+
+   sgMail.setApiKey(sendGrdiKey);
+   const msg = {
       to: email,
-      subject: "thank you",
-      html: `<h3>Password was updated successfully!</h3>`
-   });
-   console.log("Message sent: %s", info.messageId);
+      from: sender, 
+      subject: "Thank you",
+      text: 'important',
+      html: `<h3>Password was updated successfully!</h3>`,
+   }
+   sgMail
+      .send(msg)
+      .then(() => {
+         console.log('Email sent')
+      })
+      .catch((error: any) => {
+         console.error(error)
+      })
 }
+
+
+
 
 export {
    resetPasswordEmail,
